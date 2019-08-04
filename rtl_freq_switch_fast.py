85634aa4819a90cca68f902afd4e816f8e7ed61c
Evan Mayer <evanmayer@uchicago.edu>
2019-08-04 11:46:35 -0700

    Re-implemented frequency switched integration to average spectra correctly. Implemented spectrum folding to arrive at the expected result for a fully processed frequency switched spectrum. Minor changes to switch_fast. Probably going to go away soon.

--- rtl_freq_switch_fast.py
+++ rtl_freq_switch_fast.py
@@ -18,7 +18,7 @@ from rtlsdr import RtlSdr
 
 def run_fswitch( NFFT, gain, rate, fc, fthrow, fswitch ):
     '''
-    NFFT:    Pass-through for matplotlib plot psd: powers of 2 are most efficient
+    NFFT:    Pass-trough for matplotlib plot psd: powers of 2 are most efficient
     gain:    Requested SDR gain (dB)
     rate:    SDR sample rate, intrinsically tied to bandwidth in SDRs (Hz_
     fc:      base center frequency (Hz)
@@ -30,7 +30,7 @@ def run_fswitch( NFFT, gain, rate, fc, fthrow, fswitch ):
     # Initial plotting
     fig, ax = plt.subplots()
     plt.grid()
-    ax.set_ylim(-1e-4, 1e-4) 
+    ax.set_ylim(-1e-4, 1e-4)
 
     sdr = RtlSdr()
     sdr.rs = rate # Rate of Sampling (intrinsically tied to bandwidth with SDR dongles)
@@ -43,13 +43,9 @@ def run_fswitch( NFFT, gain, rate, fc, fthrow, fswitch ):
     # The amount of time to integrate on each frequency
     delta_t = 1./(2.*fswitch)
 
-    # The number of iq samples to read on each call to the RtlSdr
-    NUM_READ_SAMPLES = NFFT * 1000
-
-    iq = sdr.read_samples(NUM_READ_SAMPLES) # get initial data from sdr
+    iq = sdr.read_samples(NFFT) # get initial data from sdr
     p_xx, freqs, psdlines = plt.psd(iq, NFFT=NFFT, Fc=sdr.fc/1e6, Fs=sdr.rs/1e6, return_line=True, animated=True)
     line = psdlines[0]
-    line.linewidth=0.5
 
 
     '''
@@ -63,18 +59,18 @@ def run_fswitch( NFFT, gain, rate, fc, fthrow, fswitch ):
 
 
     '''
-    wraps read_samples, the pyrtlsdr data getter, to pass new y-vals into 
+    wraps read_samples, the pyrtlsdr data getter, to pass new y-vals into
     animation below
 
     Integrates frequency-switched data.
-    
+
     inputs: sdr, the pyrtlsdr object
     outputs: psd_y, a generator that gives the ydata of the psd plot.
     '''
     def data_gen():
         # Ensure set to base frequency at start of every cycle
         sdr.fc = fc
-        
+
         # Set the time: the start of the integration cycles
         start_time = time.time()
 
@@ -83,10 +79,10 @@ def run_fswitch( NFFT, gain, rate, fc, fthrow, fswitch ):
         iq_on = []
         while time.time()-start_time < delta_t:
             # Collect on-frequency samples
-            iq_on.extend(sdr.read_samples(NUM_READ_SAMPLES))
+            iq_on.extend(sdr.read_samples(NFFT))
         # Take the PSD of the on samples
-        p_xx_on, freqs_on = psd(iq_on, NFFT=NFFT, Fs=sdr.rs/1e6)    
-        p_xx_on_tot += p_xx_on                                 
+        p_xx_on, freqs_on = psd(iq_on, NFFT=NFFT, Fs=sdr.rs/1e6)
+        p_xx_on_tot += p_xx_on
 
         # Switch to the switched frequency
         sdr.fc = fc + fthrow
@@ -98,23 +94,23 @@ def run_fswitch( NFFT, gain, rate, fc, fthrow, fswitch ):
         iq_off = []
         while time.time()-start_time < delta_t:
             # Collect off-frequency samples
-            iq_off.extend(sdr.read_samples(NUM_READ_SAMPLES))
+            iq_off.extend(sdr.read_samples(NFFT))
         # And the off samples
         p_xx_off, freqs_off = psd(iq_off, NFFT=NFFT, Fs=sdr.rs/1e6)
-        p_xx_off_tot += p_xx_off      
-            
+        p_xx_off_tot += p_xx_off
+
         if not p_xx_on_tot.any() or not p_xx_off_tot.any():
             print('Warning: One of the frequency-switched spectra was empty. Consider lowering the switching frequency in order for RTL-SDR sampling to catch up.')
         # Add folded samples to the current and update
         p_xx_tot = (p_xx_on_tot - p_xx_off_tot)/2.
 
-        yield p_xx_tot 
+        yield p_xx_tot
 
 
     # play animation
     ani = animation.FuncAnimation(fig, update, data_gen, interval=0, blit=True)
     plt.show()
-    
+
     # nice and tidy
     sdr.close()
 
@@ -126,10 +122,10 @@ if __name__ == '__main__':
     parser.add_argument('-g', '--gain', dest='gain', default=30.0, action='store', type=float, help='Requested SDR gain (dB)')
     parser.add_argument('-r', '--rate', dest='rate', default=2.56e6, action='store', type=float, help='Sample rate (Hz)')
     parser.add_argument('-c', '--fc', dest='fc', default=1.4202e9, action='store', type=float, help='Requested base frequency (Hz)')
-    parser.add_argument('-d', '--dfthrow', dest='fthrow', default=0.0004e9, action='store', type=float, help='Delta to fc applied on every switching integration cycle. (Hz)')
+    parser.add_argument('-t', '--fthrow', dest='fthrow', default=0.0004e9, action='store', type=float, help='Delta to fc applied on every switching integration cycle. (Hz)')
     parser.add_argument('-s', '--fswitch', dest='fswitch', default=0.2, action='store', type=float, help='Frequency at which SDR tuning frequency is switched from fc to fc+fthrow. One cycle is from fc to fc+fthrow bck to fc. (Hz)')
     args = parser.parse_args()
 
-    NFFT=2048
+    NFFT=1024
 
     run_fswitch( NFFT, args.gain, args.rate, args.fc, args.fthrow, args.fswitch )
--- rtl_freq_switch_int.py
+++ rtl_freq_switch_int.py
@@ -9,15 +9,13 @@ Integrate over a specified length of time and save off to a text file.
 # Import plotting necessities
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from matplotlib.mlab import psd
 import time
 
 from rtlsdr import RtlSdr
 
 
-def run_fswitch( NFFT, gain, rate, fc, fthrow, fswitch, t_int ):
+def run_fswitch( NFFT, gain, rate, fc, fthrow, t_int ):
     '''
     Inputs:
     NFFT:    Pass-through for matplotlib plot psd: powers of 2 are most efficient
@@ -26,12 +24,12 @@ def run_fswitch( NFFT, gain, rate, fc, fthrow, fswitch, t_int ):
     fc:      base center frequency (Hz)
     fthrow:  frequency to switch to, samples subtracted from spectrum of fc
              (Hz)
-    fswitch: switching rate (Hz)
     t_int:   total integration time (s)
 
     Returns:
-    freqs:   Frequencies of the resulting spectrum, centered at fc (Hz), numpy array
-    p_xx_tot: ( Power spectral density on - power spectral density off ) / 2 (dB/Hz) numpy array
+    freqs_fold:  Frequencies of the resulting spectrum, centered at fc (Hz), numpy array
+    p_xx_fold:   Folded frequency-switched spectrum (dB/Hz) numpy array,
+                 following Herschel docs at http://herschel.esac.esa.int/hcss-doc-15.0/load/hifi_um/html/hcb_pfsw.html
     '''
 
     sdr = RtlSdr()
@@ -42,60 +40,72 @@ def run_fswitch( NFFT, gain, rate, fc, fthrow, fswitch, t_int ):
     print('  center frequency %0.6f MHz' % (sdr.fc/1e6))
     print('  gain: %d dB' % sdr.gain)
 
-    # The amount of time to integrate on each frequency
-    delta_t = 1./(2.*fswitch)
-
     # The number of iq samples to read on each call to the RtlSdr
-    NUM_READ_SAMPLES = NFFT #* 1000
+    NUM_READ_SAMPLES = NFFT
 
     # Set up arrays to store integrated power from psd
     p_xx_on_tot = np.zeros(NFFT)
     p_xx_off_tot = np.zeros(NFFT)
-    
+    cnt_on = 0
+    cnt_off = 0
+
     # Set the baseline time
     start_time = time.time()
     print('Integration began at {}'.format(time.strftime('%a, %d %b %Y %H:%M:%S', time.localtime(start_time))))
-
     # Time integration loop
+    # TODO: This could absolutely be sped up using a smart buffer, but this is python after all
+    # If you really need speed, just use rtl_power_fftw, that's good C++ with a buffer
     while time.time()-start_time < t_int:
-        # The start of the on-frequency cycles
-        start_time_on = time.time()
-        # Integrate on the target frequency
-        iq_on = []
-        while time.time()-start_time_on < delta_t:
-            # Collect on-frequency samples
-            iq_on.extend(sdr.read_samples(NUM_READ_SAMPLES))
-        # Take the PSD of the on samples
-        p_xx_on, freqs_on = psd(iq_on, NFFT=NFFT, Fs=rate)
-        p_xx_on_tot += p_xx_on                                 
+        # Switch to the center frequency
+        sdr.fc = fc
+        # Collect on-frequency samples
+        iq_on = np.zeros(NFFT, dtype=complex)
+        iq_on += sdr.read_samples(NUM_READ_SAMPLES)
+        if iq_on.any():
+            cnt_on += 1
 
         # Switch to the switched frequency
         sdr.fc = fc + fthrow
+        # Collect off-frequency samples
+        iq_off = np.zeros(NFFT, dtype=complex)
+        iq_off += sdr.read_samples(NUM_READ_SAMPLES)
+        if iq_off.any():
+            cnt_off += 1
 
-        start_time_off = time.time()
-        # Integrate on the switched frequency
-        iq_off = []
-        while time.time()-start_time_off < delta_t:
-            # Collect off-frequency samples
-            iq_off.extend(sdr.read_samples(NUM_READ_SAMPLES))
-        # And the off samples
+        # Take the PSD of the on samples
+        p_xx_on, freqs_on = psd(iq_on, NFFT=NFFT, Fs=rate)
+        p_xx_on_tot += p_xx_on
+        # Take the PSD of the off samples
         p_xx_off, freqs_off = psd(iq_off, NFFT=NFFT, Fs=rate)
-        p_xx_off_tot += p_xx_off      
-            
-        if not p_xx_on_tot.any() or not p_xx_off_tot.any():
-            print('Warning: One of the frequency-switched spectra was empty. Consider lowering the switching frequency in order for RTL-SDR sampling to catch up.')
+        p_xx_off_tot += p_xx_off
+
     end_time = time.time()
     print('Integration ended at {} after {} seconds.'.format(time.strftime('%a, %d %b %Y %H:%M:%S'), end_time-start_time))
+    print('{} spectra were measured at {}.'.format(cnt_on, fc))
+    print('{} spectra were measured at {}.'.format(cnt_off, fc+fthrow))
 
-    # Add folded samples to the current and update
-    p_xx_tot = (p_xx_on_tot - p_xx_off_tot)/2.
+    # Compute the average power spectrum based on the number of spectra read
+    p_avg_on = p_xx_on_tot / cnt_on
+    p_avg_off = p_xx_off_tot / cnt_off
+    # Compute the difference spectrum
+    p_xx_diff = p_avg_on - p_avg_off
+
     # Shift frequency spectrum back to the intended range
     freqs_on = freqs_on + fc
+
+    # Shift-and-add to fold
+    # Magnitude of frequency shift in bin space:
+    bin_throw = np.where(freqs_on == fc)[0][0] - np.where(freqs_on == fc-fthrow)[0][0]
+    # Subtract first NFFT-bin_throw bins from last NFFT-bin_throw bins and average
+    p_xx_fold = (p_xx_diff[bin_throw:-1] - p_xx_diff[0:NFFT-bin_throw-1]) / 2.
+    # Get the folded, upper segment of freqs to return,
+    # throwing away the unfolded part of the spectrum. Is there a way around this?
+    freqs_fold = freqs_on[bin_throw:-1]
 
     # nice and tidy
     sdr.close()
 
-    return freqs_on, p_xx_tot
+    return freqs_fold, p_xx_fold
 
 
 def save_spectrum(filename, freqs, p_xx):
@@ -103,7 +113,8 @@ def save_spectrum(filename, freqs, p_xx):
     Save the results of integration to a file.
     '''
     header='\n\n\n\n\n'
-    np.savetxt(filename, np.column_stack((freqs, p_xx)), delimiter=' ',)
+    np.savetxt(filename, np.column_stack((freqs, p_xx)), delimiter=' ', header=header)
+    print('Results were written to {}.'.format(filename))
 
     return
 
@@ -114,7 +125,6 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--rate', dest='rate', default=2.56e6, action='store', type=float, help='Sample rate (Hz), default 2.56e6 Hz')
     parser.add_argument('-c', '--fc', dest='fc', default=1.4202e9, action='store', type=float, help='Requested base frequency (Hz), default = 1.420e9 Hz')
     parser.add_argument('-d', '--dfthrow', dest='fthrow', default=4.0e5, action='store', type=float, help='Delta to fc applied on every switching integration cycle (Hz), default 4.0e5 Hz')
-    parser.add_argument('-s', '--fswitch', dest='fswitch', default=0.2, action='store', type=float, help='Frequency at which SDR tuning frequency is switched from fc to fc+fthrow. One cycle is from fc to fc+fthrow bck to fc (Hz), default 0.2 Hz')
     parser.add_argument('-t', '--time', dest='t_int', default=1, action='store', type=float, help='Total amount of time to integrate frequency switched measurements (s) default 1 s')
     parser.add_argument('-o', '--output', dest='output', default='rtl_freq_switch_int.tsv', action='store', type=str, help='Filename to save integrated spectrum to. Default is tab delimited, rtl_freq_switch_int.tsv.')
 
@@ -122,6 +132,6 @@ if __name__ == '__main__':
 
     NFFT=512
 
-    freqs, p_xx_tot = run_fswitch( NFFT, args.gain, args.rate, args.fc, args.fthrow, args.fswitch , args.t_int )
+    freqs, p_xx_tot = run_fswitch( NFFT, args.gain, args.rate, args.fc, args.fthrow, args.t_int )
     save_spectrum( args.output, freqs, p_xx_tot )
 
